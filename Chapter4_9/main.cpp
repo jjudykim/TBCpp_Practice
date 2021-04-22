5f391ac0b3a283f0a784e6e78402c0db01155595
jjudy_kim <imjubaaakim@gmail.com>
2021-04-22 16:51:02 +0900

    Chapter4_9 done.

---
+++ Chapter4_9/main.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<vector>
+//#include<cstdint>
+//
+//int main()
+//{
+//	typedef double distance_t;
+//
+//	double		my_distance;
+//	distance_t	home2work;
+//	distance_t	home2school;
+//
+//	return 0;
+//}
+
+#include<iostream>
+#include<vector>
+
+int main()
+{
+	using namespace std;
+	vector<pair<string, int> > pairlist1;
+	//아무튼 string과 int의 pair가 vector로 존재한다..
+
+	vector<pair<string, int> > pairlist2;
+	vector<pair<string, int> > pairlist3;
+	// ...
+
+	// 이런 경우도
+	typedef vector<pair<string, int> > pairlist_t;
+
+	pairlist_t pairlist11;
+	pairlist_t pairlist22;
+
+	// 이렇게도 사용이 가능하다!
+	using parilist_t = vector<pair<string, int> >;
+}
