4cfaa10c29b43c9c718284bfce4c7b0d0d24e107
jjudy_kim <imjubaaakim@gmail.com>
2021-04-24 20:47:56 +0900

    Chapter5_1 done.

---
+++ Chapter5_1/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+
+int main()
+{
+	using namespace std;
+
+	cout << "I love you " << endl;
+
+	// 나는 안 사랑하는데.. 도망가고싶어..!!
+
+	exit(0); // 프로그램 종료(굉장히 긴급하게!!)
+	return 0; // main 종료
+
+	cout << "I love you, too" << endl;
+
+	return 0;
+}
