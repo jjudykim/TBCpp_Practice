b1bef0cf67d9efaa2f01abb6420a35821009b2fc
jjudy_kim <imjubaaakim@gmail.com>
2021-04-25 23:52:54 +0900

    Chapter5_2 ing...

---
+++ Chapter5_2/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+
+	if (x > 10) cout << x << "is greater than 10" << endl;
+	else cout << x << "is not greater than 10" << endl;
+
+	return 0;
+}
