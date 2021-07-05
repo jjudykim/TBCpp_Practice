83e709edac3dc65c65df79ce71a48d35470bee93
jjudy_kim <imjubaaakim@gmail.com>
2021-07-06 01:17:15 +0900

    Chapter 6_10 done.

---
+++ Chapter6_10/소스.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	const char* name = "Jack Jack";
+//	const char* name2 = "Jack Jack";
+//
+//	cout << (uintptr_t)name << endl;
+//	cout << (uintptr_t)name2 << endl;
+//
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int int_arr[5] = { 1, 2, 3, 4, 5 };
+//	char char_arr[] = "Hello, World!";
+//	const char* name = "Jack Jack";
+//
+//	cout << int_arr << endl;
+//	cout << char_arr << endl;
+//	cout << name << endl;
+//}
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char c = 'Q';
+	cout << &c << endl;     // 아항~ 문자열인가부다!
+}
+
