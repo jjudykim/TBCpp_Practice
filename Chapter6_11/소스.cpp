acbc7d3ff9f66439dafb6c47f15ec04f4e31463f
jjudy_kim <imjubaaakim@gmail.com>
2021-07-06 16:36:24 +0900

    Chapter 6_11 done.

---
+++ Chapter6_11/소스.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	while (true)
+//	{
+//		int* ptr = new int;
+//		cout << ptr << endl;
+//	}
+//}
+
+// 무한으로 즐겨요~ 명륜진사메모리,,
